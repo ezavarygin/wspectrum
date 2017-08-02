d3882adc89e39be1332bf4dd78b12a2474c060fc
Evgeny Zavarygin <e.zavarygin@gmail.com>
2017-08-02 18:06:28 +1000

    Minor changes

--- functions/functions_weighted.py
+++ functions/functions_weighted.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from astropy.io import fits
+from glob import glob
 import numpy as np
 
 
@@ -39,6 +40,90 @@ def find_anchor(file_list,shift_path):
         print "========================================================================="
         quit()
     return file_list[i_an]
+
+
+def group_print(file_list):
+    """
+    The module is to print names of spectra by groups.
+
+    The spectra are grouped according to instrumental set-ups: binning and deckname.
+    """
+    print ""
+    print "=============== wspectrum ==============="
+    print ""
+    print "'--group' was specified!"
+    print ""
+    print "Spectra grouped by instrumental set-ups:"
+    print "........................................."
+    spec_setup_dict = {}
+    for path in file_list:
+        name = path.split('/')[-1]
+        # find a path to the corresponding raw fits-file
+        find_path = glob('/Users/z5093467/ASTRO/reduction/HIRES/J101155+294141/HI.????????/sci/' + name)[0]
+        fits_open = fits.open(find_path)
+        deckname  = fits_open[0].header['deckname']
+        if len(fits_open) == 4:
+            binning = fits_open[0].header['binning'][-1]
+        else:
+            binning = fits_open[0].header['binning'][0]
+        spec_setup_dict[name] = [str(binning),str(deckname)]
+    fits_open.close()
+    name_list = spec_setup_dict.keys()
+    while len(name_list) > 0:
+        spec_name = name_list[0]
+        binning   = spec_setup_dict[spec_name][0]
+        deckname  = spec_setup_dict[spec_name][1]
+        print "Set-ups: binning = {}, slit = {}".format(binning,deckname)
+        print ""
+        for key, value in sorted(spec_setup_dict.iteritems()):
+            if value[0] == binning and value[1] == deckname:
+                print key #, value
+                name_list.remove(key)
+            else:
+                pass
+        print "........................................."
+
+def voffset_bin(path_to_file):
+    """
+    The module is to extend 'voffset' output file with instrument set-ups. 
+
+    The file voffset_setups.dat is created which contains 4 columns:
+    (1) file name of a spectrum, 
+    (2) velocity off-set,
+    (3) binning in the dispersion direction,
+    (4) decker name.
+    """
+    with open(path_to_file,'r') as f:
+        data = f.read().split('\n')
+    if path_to_file.split('/')[-1] == 'voffset_setups.dat':
+        print "=============================================================================="
+        print ""
+        print "Warning: 'voffset_setups.dat' is reserved for the output file!"
+        print ""
+        print "Change either the input file name, or rename an output file name."
+        print "In order to do the latter, modify a corresponding line in the"
+        print "'funcions_weighted.voffset_bin' module!"
+        print "=============================================================================="
+        quit()
+    else:
+        pass
+    new_file = open('voffset_setups.dat','w')
+    for i,line in enumerate(data):
+        name = line.split()[0]
+        # find a path to the corresponding raw fits-file
+        find_path = glob('/Users/z5093467/ASTRO/reduction/HIRES/J101155+294141/HI.????????/sci/' + name)[0]
+        fits_open = fits.open(find_path)
+        deckname = fits_open[0].header['deckname']
+        if len(fits_open) == 4:
+            binning = fits_open[0].header['binning'][-1]
+        else:
+            binning = fits_open[0].header['binning'][0]
+        if i != len(data)-1:
+            new_file.write(line + ' ' + str(binning) + ' ' + str(deckname) + '\n')
+        else:
+            new_file.write(line + ' ' + str(binning) + ' ' + str(deckname))
+    fits_open.close()
+    new_file.close()
 
 
 def detail_wl_array(old_array,resol):
